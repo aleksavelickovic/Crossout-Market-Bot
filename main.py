a6332ed6cae59111ed2acb13870407317cd4b835
aleksavelickovic <velickovic.sr44.2023@uns.ac.rs>
2025-05-09 01:21:30 +0200

    Refactor code structure and remove redundant functions for improved readability and maintainability

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ def adjust_buy_order(current_price, item_coords, i):
         market_buy_price = read_price_from_screen(region=BUY_PRICE_REGION)
         market_sell_price = read_price_from_screen(region=SELL_PRICE_REGION)
 
-        if ((market_sell_price * 0.90) - market_buy_price) < 15:                 # VRATITI U PRVOBITNO STANJE NAKON PRODVANJA IZABRANOG DELA (VRACENO)
+        if ((market_sell_price * 0.90) - market_buy_price) < 25:                 # VRATITI U PRVOBITNO STANJE NAKON PRODVANJA IZABRANOG DELA (VRACENO)
             print("Profit would be too small to raise the price!")
             keyboard.press_and_release("esc")
             with open("buy-order-log.txt", "a") as log_file:
